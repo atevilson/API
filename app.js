688d458b5e87a298abad89ad86adc5f54ea0055a
atevilson <freitas.atevilson@gmail.com>
2022-12-07 20:30:18 -0300

    add implementação da primeira rota e uma variável com dados em memória

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+
+const carros = [
+
+{id: 1, "modelo": "Onix Plus", "Motor": "Turbo", "Cor": "Azul", "Potência": "116cv", "Ano": "2023", "Valor": 86.000},
+{id: 2, "modelo": "Cruze Sedan", "Motor": "Turbo", "Cor": "Cinza Satin", "Potência": "153cv", "Ano": "2023", "Valor": 145.020}
+
+];
+
+
+// Rota de pesquisa
+
+app.get("/carros", (req, res) => {
+    res.status(200).json(carros);
+});
---
+++ server.js
@@ -0,0 +1,3 @@
+const port = process.env.port || 3000;
+
+console.log(`Server rodando no endereço: http://localhost:${port}`);
