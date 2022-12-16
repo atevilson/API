8774728b2ef96fd50c5d44a416ad94b8d54ad38a
atevilson <freitas.atevilson@gmail.com>
2022-12-16 19:59:43 -0300

    removendo rotas e array de memória do módulo app

--- src/app.js
+++ src/app.js
@@ -13,28 +13,4 @@ const app = express();
 app.use(express.json());
 
 
-const carros = [
-
-{id: 1, "Modelo": "Onix Plus", "Motor": "Turbo", "Cor": "Azul", "Potência": "116cv", "Ano": "2023", "Valor": "R$ 86.000"},
-{id: 2, "Modelo": "Cruze Sedan", "Motor": "Turbo", "Cor": "Cinza Satin", "Potência": "153cv", "Ano": "2023", "Valor": "R$ 145.020"}
-
-];
-
-
-// Rota de pesquisa
-
-app.get("/carros", (req, res) => {
-    res.status(200).json(carros);
-});
-
-// Rota de pesquisa por id
-app.get("/carros:id", (req, res) => {
-    res.status(200).json(carros);
-});
-
-app.post("/carros", (req, res) => {
-    carros.push(req.body);
-    res.status(201).send("Carro cadastrado!");
-});
-
 export default app;
