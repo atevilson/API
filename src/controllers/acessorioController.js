553070dd7b4ae193130ced572352eaa4a83f4bf0
atevilson <freitas.atevilson@gmail.com>
2022-12-19 21:48:46 -0300

    add controller para as rotas de acessorio

---
+++ src/controllers/acessorioController.js
@@ -0,0 +1,62 @@
+import acessorio from "../models/Acessorio.js";
+
+class AcessorioController {
+    // código para buscar acessorios
+    static listarAcessorios = (req, res) => {
+        acessorio.find((err, acessorio) => {
+            res.status(200).send(acessorio)
+        });
+    }
+
+    // código para busca de acessorios por id
+    static buscarAcessoriosPorId = (req, res) => {
+        const id = req.params.id;
+
+        acessorio.findById(id, (err, acessorio) => {
+            if(err) {
+                res.status(400).send({message: `${err.message} - Id não existe`});
+            } else {
+                res.status(200).send(acessorio);
+            }
+        });
+    }
+
+    // código para cadastrar acessorios
+    static cadastroDeAcessorios = (req, res) => {
+        let acessorio = new acessorio(req.body);
+        acessorio.save((err) => {
+            if(err) {
+                res.status(500).send({message: `${err.message}, Não foi possível cadastrar o acessorio!`});
+            } else {
+                res.status(201).send(acessorio.toJSON());
+            }
+        });
+    }
+
+    // código para atualizar acessorios por id
+    static atualizarAcessorios = (req, res) => {
+        const id = req.params.id;
+
+        acessorio.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+            if(!err) {
+                res.status(200).send({message: "Acessorio atualizado com sucesso"});
+            } else {
+                res.status(500).send({message: err.message});
+            }
+        });
+    }
+
+    // código para deletar acessorios por id
+    static deletarAcessorios = (req, res) => {
+        const id = req.params.id;    
+        
+        acessorio.findByIdAndDelete(id, (err) => {
+            if(!err) {
+                res.status(200).send({message: "Acessorio excluído com sucesso"});
+            } else {
+                res.status(500).send({message: err.message});
+            }
+        });
+
+    }
+}
