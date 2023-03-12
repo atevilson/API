0701f0decf27566916692f296503546bef898877
atevilson <freitas.atevilson@gmail.com>
2023-03-11 23:26:15 -0300

    add pacote jest para testes unitários

--- src/app.js
+++ src/app.js
@@ -2,11 +2,10 @@ import express from "express";
 import db from "./database/dbConnect.js";
 import routes from "./routes/index.js";
 
-// Conexão do banco com a API
-
+/* Conexão do banco com a API */
 db.on("error", console.log.bind(console, "Erro na conexão com o banco de dados!"));
 db.once("open", () => {
-    console.log("Conexão com o banco bem sucedida!");
+  console.log("Conexão com o banco bem sucedida!");
 });
 
 const app = express();
